3ec968611f1a9c1a6dda7a93f1235a2c43ca93eb
Aditya Garg <61191738+AdityaGarg00@users.noreply.github.com>
2021-11-07 15:17:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'cms-adi.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms-adi'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'CMS'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'aditya'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'P@ssword'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
